8f00801febb5f17ca05b6a6938fe2a5d7528ac31
aalole <abiodunrasheed93@gmail.com>
2019-05-06 13:09:30 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var a = 2;
+var c = 3;
+var d = 4;
+var e= 5;
+var f = 6;
+var b = [1,2,3,4,5,6,7,8,9,10];
+document.write (b.map(num=> a*num)); +
+document.write (b.map(num=> c*num));
+document.write (b.map(num=> d*num));
+document.write (b.map(num=> e*num));
+document.write (b.map(num=> f*num));
+
+
+
+
+
